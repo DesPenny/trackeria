b9d77c8034266f09bb31dca4e7c4c18cf26b7f6f
Eric Carter <ericcarter103@gmail.com>
2013-05-03 00:43:34 -0400

    annotated

--- spec/models/history_spec.rb
+++ spec/models/history_spec.rb
@@ -1,3 +1,21 @@
+# == Schema Information
+#
+# Table name: histories
+#
+#  id           :integer          not null, primary key
+#  domainurl_id :integer
+#  domainurl    :string(255)
+#  keyword      :string(255)
+#  category     :string(255)
+#  google_rank  :integer
+#  bing_rank    :integer
+#  yahoo_rank   :integer
+#  alexa_global :integer
+#  page_rank    :integer
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
 require 'spec_helper'
 
 describe History do
