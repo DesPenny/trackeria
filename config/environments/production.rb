73a56c2b4b25b7155e12e0ec403f1925fb5691c2
Eric Carter <ericcarter103@gmail.com>
2013-05-08 21:28:35 -0400

    email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,13 +1,13 @@
 Rankapp::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
-  config.action_mailer.default_url_options = { :host => 'trackeria.com' }
+  config.action_mailer.default_url_options = { :host => 'http://trackeria.com' }
   ActionMailer::Base.smtp_settings = {
     :address        => "smtp.sendgrid.net",
     :port           => "25",
     :authentication => :plain,
     :user_name      => ENV['app15427778@heroku.com'],
     :password       => ENV['2gob2gtt'],
-    :domain         => ENV['trackeria.com']
+    :domain         => ENV['http://trackeria.com']
   }
   # Code is not reloaded between requests
   config.cache_classes = true
