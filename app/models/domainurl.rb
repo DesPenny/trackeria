774d3bf7480999465eaaefe3e381124ef1f455d1
Eric Carter <ericcarter103@gmail.com>
2013-05-14 13:33:39 -0400

    scheduler

--- app/models/domainurl.rb
+++ app/models/domainurl.rb
@@ -200,7 +200,7 @@ class Domainurl < ActiveRecord::Base
 
   def Domainurl.update_domainurl_failed
     Domainurl.all.each do |f|
-      if f.updatecount!=1
+      if f.updatecount!=0
           if f.googlesource=="USA"
               f.yahoo_rank=Ranking.new(:keyword => "#{f.keyword}", :url =>  "#{f.domainurl}", :limit => 200).from_yahoo
               f.bing_rank=Ranking.new(:keyword => "#{f.keyword}", :url =>  "#{f.domainurl}", :limit => 200).from_bing
